6b8ec936b7399d4fbb19ff347baec3cb1e42adfa
Brooke Kuhlmann <brooke@alchemists.io>
2015-09-12 12:29:47 -0600

    Added gem configuration error support.
    - Defines a specific error class for use with
      invalid configurations.
    - This will make more sense when the configuration class
      for the gem is added next where custom settings can be
      defined for the gem.

--- lib/milestoner.rb
+++ lib/milestoner.rb
@@ -1,5 +1,6 @@
 require "milestoner/aids/git"
 require "milestoner/errors/base"
+require "milestoner/errors/configuration"
 require "milestoner/errors/duplicate_tag"
 require "milestoner/errors/git"
 require "milestoner/errors/version"
---
+++ lib/milestoner/errors/configuration.rb
@@ -0,0 +1,7 @@
+module Milestoner
+  module Errors
+    # Raised for invalid gem configuration.
+    class Configuration < Base
+    end
+  end
+end
