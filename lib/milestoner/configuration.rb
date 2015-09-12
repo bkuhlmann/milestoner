57d134e1bd4866529be18e6e158c0a3aaa8d6a5c
Brooke Kuhlmann <brooke@alchemists.io>
2015-09-12 12:31:42 -0600

    Added global and local gem configuration support.
    - Adds the Configuration class which manages the loading of
      gem configuration settings via global or local settings.
    - Default configuration files paths are:
      - Global: ~/.milestonerrc
      - Local: <current working directory>/.milestonerrc
    - Configuration load order is local first then global.
      If both local and global configuration files can't be
      found, then default settings are used instead.

---
+++ lib/milestoner/configuration.rb
@@ -0,0 +1,36 @@
+module Milestoner
+  # Default configuration for gem with support for custom settings.
+  class Configuration
+    def initialize file_name = ".unknownrc", defaults: {}
+      @file_name = file_name
+      @defaults = defaults
+    end
+
+    def local_file_path
+      File.join Dir.pwd, file_name
+    end
+
+    def global_file_path
+      File.join ENV["HOME"], file_name
+    end
+
+    def computed_file_path
+      File.exist?(local_file_path) ? local_file_path : global_file_path
+    end
+
+    def settings
+      defaults.merge(load_settings).reject { |_, value| value.nil? }
+    end
+
+    private
+
+    attr_reader :file_name, :defaults
+
+    def load_settings
+      yaml = YAML.load_file computed_file_path
+      yaml.is_a?(Hash) ? yaml : {}
+    rescue
+      defaults
+    end
+  end
+end
---
+++ spec/lib/milestoner/configuration_spec.rb
@@ -0,0 +1,126 @@
+require "spec_helper"
+
+describe Milestoner::Configuration, :temp_dir do
+  let(:file_name) { ".testrc" }
+  subject { described_class.new }
+
+  describe "#global_file_path" do
+    it "answers default file path" do
+      expect(subject.global_file_path).to eq(File.join(ENV["HOME"], ".unknownrc"))
+    end
+
+    it "answers custom file path" do
+      subject = described_class.new file_name
+      expect(subject.global_file_path).to eq(File.join(ENV["HOME"], file_name))
+    end
+  end
+
+  describe "#local_file_path" do
+    it "answers default file path" do
+      expect(subject.local_file_path).to eq(File.join(Dir.pwd, ".unknownrc"))
+    end
+
+    it "answers custom file path" do
+      subject = described_class.new file_name
+      expect(subject.local_file_path).to eq(File.join(Dir.pwd, file_name))
+    end
+  end
+
+  describe "#computed_file_path" do
+    context "when local configuration exists" do
+      let(:local_file) { File.join temp_dir, ".unknownrc" }
+      before { FileUtils.touch local_file }
+
+      it "answers local file path" do
+        Dir.chdir temp_dir do
+          expect(subject.computed_file_path).to eq(subject.local_file_path)
+        end
+      end
+    end
+
+    context "when local configuration doesn't exists" do
+      it "answers global file path" do
+        Dir.chdir temp_dir do
+          expect(subject.computed_file_path).to eq(subject.global_file_path)
+        end
+      end
+    end
+  end
+
+  describe "#settings" do
+    let(:global_file) { File.join temp_dir, file_name }
+    let(:local_dir) { File.join temp_dir, "local" }
+    let(:local_file) { File.join local_dir, file_name }
+    let(:defaults) { {} }
+    let(:global_defaults) { {example: "Global"} }
+    let(:local_defaults) { {example: "Local"} }
+    subject { described_class.new file_name, defaults: defaults }
+
+    context "when using global path" do
+      before { File.open(global_file, "w") { |file| file << global_defaults.to_yaml } }
+
+      it "answers global settings" do
+        ClimateControl.modify HOME: temp_dir do
+          expect(subject.settings).to eq(global_defaults)
+        end
+      end
+    end
+
+    context "when using local path" do
+      before do
+        FileUtils.mkdir_p local_dir
+        File.open(global_file, "w") { |file| file << global_defaults.to_yaml }
+        File.open(local_file, "w") { |file| file << local_defaults.to_yaml }
+      end
+
+      it "answers local settings" do
+        ClimateControl.modify HOME: temp_dir do
+          Dir.chdir local_dir do
+            expect(subject.settings).to eq(local_defaults)
+          end
+        end
+      end
+    end
+
+    context "when configuration file doesn't exist" do
+      it "answers default settings" do
+        ClimateControl.modify HOME: temp_dir do
+          expect(subject.settings).to eq({})
+        end
+      end
+    end
+
+    context "when file has nil values" do
+      let(:global_defaults) { {one: "one", two: nil, three: "three"} }
+      before { File.open(global_file, "w") { |file| file << global_defaults.to_yaml } }
+
+      it "answers settings with values only" do
+        ClimateControl.modify HOME: temp_dir do
+          expect(subject.settings).to eq(one: "one", three: "three")
+        end
+      end
+    end
+
+    context "when defaults have nil values" do
+      let(:defaults) { {one: "one", two: nil, three: "three"} }
+
+      it "answers settings with values only" do
+        ClimateControl.modify HOME: temp_dir do
+          expect(subject.settings).to eq(one: "one", three: "three")
+        end
+      end
+    end
+
+    context "when using file and default settings" do
+      let(:global_defaults) { {two: %w(red black white), four: 4} }
+      let(:defaults) { {one: 1, two: 2, three: 3} }
+      before { File.open(global_file, "w") { |file| file << global_defaults.to_yaml } }
+
+      it "answers merged settings with file taking precedence over defaults" do
+        ClimateControl.modify HOME: temp_dir do
+          expect(subject.settings).to eq(one: 1, two: %w(red black white), three: 3, four: 4)
+        end
+      end
+    end
+  end
+end
