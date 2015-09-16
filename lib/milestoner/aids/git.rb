8b45035f93b2450a618ffda40dca580cbcbffc8b
Brooke Kuhlmann <brooke@alchemists.io>
2015-09-15 21:10:12 -0600

    Added Git aid for detecting if remote repository is configured.
    - Necessary to help prevent pushing tags to a remote repository
      that doesn't exist yet.

--- lib/milestoner/aids/git.rb
+++ lib/milestoner/aids/git.rb
@@ -5,6 +5,10 @@ module Milestoner
       def git_supported?
         File.exist? File.join(Dir.pwd, ".git")
       end
+
+      def git_remote?
+        !`git config remote.origin.url`.empty?
+      end
     end
   end
 end
--- spec/lib/milestoner/aids/git_spec.rb
+++ spec/lib/milestoner/aids/git_spec.rb
@@ -4,17 +4,39 @@ describe Milestoner::Aids::Git, :temp_dir do
   subject { instance_double("dummy").extend Milestoner::Aids::Git }
   let(:git_dir) { File.join temp_dir, ".git" }
 
-  context "when .git directory exists" do
-    before { FileUtils.mkdir_p git_dir }
+  describe "#git_supported?" do
+    context "when .git directory exists" do
+      before { FileUtils.mkdir_p git_dir }
 
-    it "answers true" do
-      Dir.chdir(temp_dir) { expect(subject.git_supported?).to eq(true) }
+      it "answers true" do
+        Dir.chdir(temp_dir) { expect(subject.git_supported?).to eq(true) }
+      end
+    end
+
+    context "when .git directory doesn't exist" do
+      it "answers false" do
+        Dir.chdir(temp_dir) { expect(subject.git_supported?).to eq(false) }
+      end
     end
   end
 
-  context "when .git directory doesn't exist" do
-    it "answers false" do
-      Dir.chdir(temp_dir) { expect(subject.git_supported?).to eq(false) }
+  describe "#git_remote?" do
+    before { Dir.chdir(temp_dir) { `git init` } }
+
+    context "when remote repository is defined" do
+      before do
+        Dir.chdir(temp_dir) { `git config remote.origin.url git@github.com:example/example.git` }
+      end
+
+      it "answers true" do
+        Dir.chdir(temp_dir) { expect(subject.git_remote?).to eq(true) }
+      end
+    end
+
+    context "when remote repository is not defined" do
+      it "answers false" do
+        Dir.chdir(temp_dir) { expect(subject.git_remote?).to eq(false) }
+      end
     end
   end
 end
