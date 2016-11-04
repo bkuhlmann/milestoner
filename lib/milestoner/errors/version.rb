307024b8c8afe1eabe64b15b8ffaddc6f10e6b6d
Brooke Kuhlmann <brooke@alchemists.io>
2016-11-04 10:47:28 -0600

    Removed `Milestoner::Errors::Version`.
    - No longer needed now that Versionaire is being used.

--- lib/milestoner/errors/version.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Milestoner
-  module Errors
-    # Raised for invalid version formats.
-    class Version < Base
-    end
-  end
-end
--- spec/lib/milestoner/errors/version_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "spec_helper"
-
-RSpec.describe Milestoner::Errors::Version do
-  subject { described_class.new }
-
-  it_behaves_like "a base error"
-end
