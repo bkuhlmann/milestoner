5e9e6ba17acd8e70239a504941820493a5b8b825
Brooke Kuhlmann <brooke@alchemists.io>
2016-11-08 11:31:10 -0700

    Updated RSpec Git repo shared context syntax.
    - Easier to read when the commands use the same options.

--- spec/support/shared_contexts/git_repo.rb
+++ spec/support/shared_contexts/git_repo.rb
@@ -2,7 +2,7 @@
 
 RSpec.shared_context "Git Repository", :git_repo do
   let(:temp_dir) { File.join Bundler.root, "tmp", "rspec" }
-  let(:git_repo_dir) { File.join temp_dir, "tester" }
+  let(:git_repo_dir) { File.join temp_dir, "repo" }
   let(:git_user_name) { "Testy Tester" }
   let(:git_user_email) { "tester@example.com" }
 
@@ -15,9 +15,9 @@ RSpec.shared_context "Git Repository", :git_repo do
       FileUtils.touch "three.txt"
       `git init`
       `rm -rf .git/hooks`
-      `git config --local user.name "#{git_user_name}"`
-      `git config --local user.email "#{git_user_email}"`
-      `git config --local core.hooksPath /dev/null`
+      `git config user.name "#{git_user_name}"`
+      `git config user.email "#{git_user_email}"`
+      `git config core.hooksPath /dev/null`
       `git config remote.origin.url git@github.com:example/example.git`
       `git add --all .`
       `git commit --all --message "Added dummy files."`
