655dee6c918acdb4553ed839d56b31ff55a44366
Brooke Kuhlmann <brooke@alchemists.io>
2016-11-13 12:10:36 -0700

    Fixed gem requirements order.
    - Ensures dependent objects are loaded first.

--- lib/milestoner.rb
+++ lib/milestoner.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
+require "milestoner/identity"
 require "milestoner/errors/base"
 require "milestoner/errors/duplicate_tag"
 require "milestoner/errors/git"
 require "milestoner/git/config"
 require "milestoner/git/kit"
-require "milestoner/cli"
-require "milestoner/identity"
-require "milestoner/publisher"
-require "milestoner/pusher"
 require "milestoner/tagger"
+require "milestoner/pusher"
+require "milestoner/publisher"
+require "milestoner/cli"
