36f55f588ac344150a933a121d047f09a0576253
Brooke Kuhlmann <brooke@alchemists.io>
2021-02-13 11:36:17 -0700

    Added Ruby garbage collection compaction
    Necessary to leverage Ruby 3.0.0 automatic garbage collection
    compaction. The first line enables automatic compaction while the
    second line ensures the current application is compatible with
    automatic compaction.
    
    You'll want to look for crash ouput similar to `ruby -e
    "Process.kill(:SEGV, $$)"` and unexpected TypeErrors. Further details
    can be found [here](https://alanwu.space/post/check-compaction).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,9 @@ require "milestoner"
 require "refinements"
 require "climate_control"
 
+GC.auto_compact = true
+GC.verify_compaction_references double_heap: true, toward: :empty
+
 using Refinements::Pathnames
 
 Pathname.require_tree __dir__, "support/shared_examples/**/*.rb"
