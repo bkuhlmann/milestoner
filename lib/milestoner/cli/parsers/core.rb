df849a7e7de3fb491b8b3a16c1d54a617a3c7c88
Brooke Kuhlmann <brooke@alchemists.io>
2021-04-20 20:09:29 -0600

    Added CLI core parser
    Necessary to provide the primary command line interface usage options.

---
+++ lib/milestoner/cli/parsers/core.rb
@@ -0,0 +1,100 @@
+# frozen_string_literal: true
+
+require "versionaire/extensions/option_parser"
+require "refinements/structs"
+
+module Milestoner
+  module CLI
+    # Handles parsing of Command Line Interface (CLI) primary options.
+    module Parsers
+      using Refinements::Structs
+
+      # Handles parsing of Command Line Interface (CLI) core options.
+      class Core
+        def self.call configuration = Configuration::Loader.call, client: CLIENT
+          new(configuration, client: client).call
+        end
+
+        def initialize configuration = Configuration::Loader.call, client: CLIENT
+          @configuration = configuration
+          @client = client
+        end
+
+        def call arguments = []
+          client.banner = "#{Identity::LABEL} - #{Identity::SUMMARY}"
+          client.separator "\nUSAGE:\n"
+          collate
+          arguments.empty? ? arguments : client.parse!(arguments)
+        end
+
+        private
+
+        attr_reader :configuration, :client
+
+        def collate = private_methods.sort.grep(/add_/).each { |method| __send__ method }
+
+        def add_config
+          client.on(
+            "-c",
+            "--config ACTION",
+            %i[edit view],
+            "Manage gem configuration. Actions: edit || view."
+          ) do |action|
+            configuration.action_config = action
+          end
+        end
+
+        def add_publish
+          client.on(
+            "-P",
+            "--publish VERSION",
+            Versionaire::Version,
+            "Tag and push milestone to remote repository."
+          ) do |version|
+            configuration.merge! action_publish: true, git_tag_version: version
+          end
+        end
+
+        def add_push
+          client.on(
+            "-p",
+            "--push VERSION",
+            Versionaire::Version,
+            "Push local tag to remote repository."
+          ) do |version|
+            configuration.merge! action_push: true, git_tag_version: version
+          end
+        end
+
+        def add_status
+          client.on "-s", "--status", "Show project status." do
+            configuration.action_status = true
+          end
+        end
+
+        def add_tag
+          client.on(
+            "-t",
+            "--tag VERSION",
+            Versionaire::Version,
+            "Tag local repository with new version."
+          ) do |version|
+            configuration.merge! action_tag: true, git_tag_version: version
+          end
+        end
+
+        def add_version
+          client.on "-v", "--version", "Show gem version." do
+            configuration.action_version = Identity::VERSION_LABEL
+          end
+        end
+
+        def add_help
+          client.on "-h", "--help", "Show this message." do
+            configuration.action_help = true
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/milestoner/cli/parsers/core_spec.rb
@@ -0,0 +1,128 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Milestoner::CLI::Parsers::Core do
+  using Versionaire::Cast
+
+  subject(:parser) { described_class.new configuration }
+
+  let(:configuration) { Milestoner::CLI::Configuration::Loader.call }
+
+  it_behaves_like "a parser"
+
+  describe "#call" do
+    it "answers config edit (short)" do
+      parser.call %w[-c edit]
+      expect(configuration.action_config).to eq(:edit)
+    end
+
+    it "answers config edit (long)" do
+      parser.call %w[--config edit]
+      expect(configuration.action_config).to eq(:edit)
+    end
+
+    it "answers config view (short)" do
+      parser.call %w[-c view]
+      expect(configuration.action_config).to eq(:view)
+    end
+
+    it "answers config view (long)" do
+      parser.call %w[--config view]
+      expect(configuration.action_config).to eq(:view)
+    end
+
+    it "fails with missing config action" do
+      expectation = proc { parser.call %w[--config] }
+      expect(&expectation).to raise_error(OptionParser::MissingArgument, /--config/)
+    end
+
+    it "fails with invalid config action" do
+      expectation = proc { parser.call %w[--config bogus] }
+      expect(&expectation).to raise_error(OptionParser::InvalidArgument, /bogus/)
+    end
+
+    it "answers publish version (short)" do
+      parser.call %w[-P 0.0.0]
+
+      expect(configuration).to have_attributes(
+        action_publish: true,
+        git_tag_version: Version("0.0.0")
+      )
+    end
+
+    it "answers publish version (long)" do
+      parser.call %w[--publish 0.0.0]
+
+      expect(configuration).to have_attributes(
+        action_publish: true,
+        git_tag_version: Version("0.0.0")
+      )
+    end
+
+    it "fails publish with invalid version" do
+      expectation = proc { parser.call %w[--publish bogus] }
+      expect(&expectation).to raise_error(OptionParser::InvalidArgument, /bogus/)
+    end
+
+    it "answers push version (short)" do
+      parser.call %w[-p 0.0.0]
+      expect(configuration).to have_attributes(action_push: true, git_tag_version: Version("0.0.0"))
+    end
+
+    it "answers push version (long)" do
+      parser.call %w[--push 0.0.0]
+      expect(configuration).to have_attributes(action_push: true, git_tag_version: Version("0.0.0"))
+    end
+
+    it "fails push with invalid version" do
+      expectation = proc { parser.call %w[--push bogus] }
+      expect(&expectation).to raise_error(OptionParser::InvalidArgument, /bogus/)
+    end
+
+    it "enables status (short)" do
+      parser.call %w[-s]
+      expect(configuration.action_status).to eq(true)
+    end
+
+    it "enables status (long)" do
+      parser.call %w[--status]
+      expect(configuration.action_status).to eq(true)
+    end
+
+    it "answers tag version (short)" do
+      parser.call %w[-t 0.0.0]
+      expect(configuration).to have_attributes(action_tag: true, git_tag_version: Version("0.0.0"))
+    end
+
+    it "answers tag version (long)" do
+      parser.call %w[--tag 0.0.0]
+      expect(configuration).to have_attributes(action_tag: true, git_tag_version: Version("0.0.0"))
+    end
+
+    it "fails tag with invalid version" do
+      expectation = proc { parser.call %w[--tag bogus] }
+      expect(&expectation).to raise_error(OptionParser::InvalidArgument, /bogus/)
+    end
+
+    it "answers version (short)" do
+      parser.call %w[-v]
+      expect(configuration.action_version).to match(/Milestoner\s\d+\.\d+\.\d+/)
+    end
+
+    it "answers version (long)" do
+      parser.call %w[--version]
+      expect(configuration.action_version).to match(/Milestoner\s\d+\.\d+\.\d+/)
+    end
+
+    it "enables help (short)" do
+      parser.call %w[-h]
+      expect(configuration.action_help).to eq(true)
+    end
+
+    it "enables help (long)" do
+      parser.call %w[--help]
+      expect(configuration.action_help).to eq(true)
+    end
+  end
+end
