889941345a95cafa81b231a15d86001331c87c60
Brooke Kuhlmann <brooke@alchemists.io>
2021-04-20 20:02:53 -0600

    Added CLI security parser
    Necessary to provide command line options for the signing or not of GPG
    tags. The truth table is being used to deal with situations in which
    the gem configuration might not be boolean when compared with CLI
    boolean input.

---
+++ lib/milestoner/cli/parsers/security.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module Milestoner
+  module CLI
+    module Parsers
+      # Handles parsing of Command Line Interface (CLI) security options.
+      class Security
+        def self.call configuration = Configuration::Loader.call, client: CLIENT
+          new(configuration, client: client).call
+        end
+
+        def initialize configuration = Configuration::Loader.call, client: CLIENT
+          @configuration = configuration
+          @client = client
+        end
+
+        def call arguments = []
+          client.separator "\nSECURITY OPTIONS:\n"
+          add_sign
+          arguments.empty? ? arguments : client.parse!(arguments)
+        end
+
+        private
+
+        attr_reader :configuration, :client
+
+        def add_sign
+          client.on(
+            "--[no-]sign",
+            %(Sign with GPG key. Default: #{configuration.git_tag_sign}.)
+          ) do |value|
+            compute_git_tag_sign value
+          end
+        end
+
+        def compute_git_tag_sign value
+          truth_table = [true, false].repeated_permutation(2).to_a
+
+          case truth_table.index [value, configuration.git_tag_sign]
+            when 0..1 then configuration.git_tag_sign = true
+            when 2..3 then configuration.git_tag_sign = false
+            else fail Error, "--sign must be a boolean. Check gem configuration."
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/milestoner/cli/parsers/security_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Milestoner::CLI::Parsers::Security do
+  using Refinements::Structs
+
+  subject(:parser) { described_class.new configuration }
+
+  include_context "with default configuration"
+
+  it_behaves_like "a parser"
+
+  describe "#call" do
+    context "when sign is disabled by default" do
+      let(:configuration) { default_configuration.merge git_tag_sign: false }
+
+      it "enables tag signing" do
+        parser.call %w[--sign]
+        expect(configuration.git_tag_sign).to eq(true)
+      end
+
+      it "disables tag signing" do
+        parser.call %w[--no-sign]
+        expect(configuration.git_tag_sign).to eq(false)
+      end
+    end
+
+    context "when sign is enabled by default" do
+      let(:configuration) { default_configuration.merge git_tag_sign: true }
+
+      it "enables tag signing" do
+        parser.call %w[--sign]
+        expect(configuration.git_tag_sign).to eq(true)
+      end
+
+      it "disables tag signing" do
+        parser.call %w[--no-sign]
+        expect(configuration.git_tag_sign).to eq(false)
+      end
+    end
+
+    context "when sign is not a boolean" do
+      let(:configuration) { default_configuration.merge git_tag_sign: nil }
+
+      it "fails with error" do
+        parse = proc { parser.call %w[--sign] }
+
+        expect(&parse).to raise_error(
+          Milestoner::Error, "--sign must be a boolean. Check gem configuration."
+        )
+      end
+    end
+  end
+end
