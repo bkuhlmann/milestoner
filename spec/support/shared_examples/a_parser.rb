1403113eb52cc832ca8fb27eb1433c91dace74f2
Brooke Kuhlmann <brooke@alchemists.io>
2021-04-20 19:11:46 -0600

    Added RSpec CLI parser shared example
    Necessary to ensure parser objects adhere to the same API.

---
+++ spec/support/shared_examples/a_parser.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples_for "a parser" do
+  describe ".call" do
+    it "answers empty array" do
+      parser = described_class.call Milestoner::CLI::Configuration::Content.new,
+                                    client: OptionParser.new
+      expect(parser).to eq([])
+    end
+  end
+end
