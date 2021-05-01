5bf41686af11d3d8f895de1468f17d54d27ccc8d
Brooke Kuhlmann <brooke@alchemists.io>
2021-05-01 17:45:34 -0600

    Refactored tagger to use commits categorizer
    Necessary to reduce duplication which is now handled by the new
    categorized object (see previous commit).

--- lib/milestoner/tagger.rb
+++ lib/milestoner/tagger.rb
@@ -1,91 +1,69 @@
 # frozen_string_literal: true
 
-require "thor"
 require "versionaire"
 
 module Milestoner
   # Handles the tagging of a project repository.
-  # :reek:TooManyInstanceVariables
   class Tagger
-    attr_reader :commit_prefixes
+    using Versionaire::Cast
 
-    def initialize commit_prefixes: [],
-                   repository: GitPlus::Repository.new,
-                   presenter: Presenters::Commit
-      @commit_prefixes = commit_prefixes
-      @repository = repository
+    def initialize categorizer: Commits::Categorizer.new,
+                   presenter: Presenters::Commit,
+                   container: Container
+
+      @categorizer = categorizer
       @presenter = presenter
-      @shell = Thor::Shell::Color.new
+      @container = container
     end
 
-    def commit_prefix_regex
-      commit_prefixes.empty? ? Regexp.new("") : Regexp.union(commit_prefixes)
-    end
+    def call configuration = CLI::Configuration::Loader.call
+      return false if local? configuration
+      fail Error, "Unable to tag without commits." if categorizer.call.empty?
 
-    def commits
-      groups = build_commit_prefix_groups
-      group_by_commit_prefix groups
-      groups.each_value { |commits| commits.sort_by!(&:subject) }
-      groups.values.flatten.uniq(&:subject)
-    end
-
-    def commit_list
-      commits.map { |source| presenter.new source }
-             .map { |commit| "- #{commit.subject_author}" }
-    end
-
-    # :reek:BooleanParameter
-    # :reek:ControlParameter
-    # :reek:TooManyStatements
-    def create version, sign: false
-      version = Versionaire::Version version
-
-      return if local? version
-      fail Errors::Git, "Unable to tag without commits." if computed_commits.empty?
-
-      content = message version
-      sign ? repository.tag_sign(version, content) : repository.tag_unsign(version, content)
+      sign configuration
+    rescue Versionaire::Errors::Cast, GitPlus::Errors::Base => error
+      raise Error, error.message
     end
 
     private
 
-    attr_reader :repository, :presenter, :shell
+    attr_reader :categorizer, :presenter, :container
 
-    def saved_commits
-      repository.commits
-    end
+    def local? configuration
+      version = Version configuration.git_tag_version
 
-    def tagged_commits
-      repository.commits "#{repository.tag_last}..HEAD"
-    end
-
-    def computed_commits
-      (repository.tagged? ? tagged_commits : saved_commits)
-    end
-
-    def build_commit_prefix_groups
-      commit_prefixes.map
-                     .with_object({}) { |prefix, group| group.merge! prefix => [] }
-                     .merge! "Other" => []
-    end
-
-    def group_by_commit_prefix groups = {}
-      computed_commits.each do |commit|
-        prefix = commit.subject[commit_prefix_regex]
-        key = groups.key?(prefix) ? prefix : "Other"
-        groups[key] << commit
+      if repository.tag_local? version
+        logger.warn "Local tag exists: #{version}. Skipped."
+        true
+      else
+        false
       end
     end
 
-    def message version
-      %(Version #{version}\n\n#{commit_list.join "\n"}\n\n)
+    def sign configuration
+      version = configuration.git_tag_version
+      content = message configuration
+
+      if configuration.git_tag_sign
+        repository.tag_sign version, content
+      else
+        repository.tag_unsign version, content
+      end
+
+      logger.debug "Local tag created: #{version}."
     end
 
-    def local? version
-      return false unless repository.tag_local? version
-
-      shell.say_status :warn, "Local tag exists: #{version}. Skipped.", :yellow
-      true
+    def message configuration
+      categorizer.call(configuration)
+                 .map { |record| presenter.new record }
+                 .map { |commit| "- #{commit.subject_author}" }
+                 .then do |commits|
+                   %(Version #{configuration.git_tag_version}\n\n#{commits.join "\n"}\n\n)
+                 end
     end
+
+    def repository = container[__method__]
+
+    def logger = container[__method__]
   end
 end
--- spec/lib/milestoner/tagger_spec.rb
+++ spec/lib/milestoner/tagger_spec.rb
@@ -3,182 +3,25 @@
 require "spec_helper"
 
 RSpec.describe Milestoner::Tagger do
-  subject(:tagger) { described_class.new commit_prefixes: %w[Fixed Added Updated Removed] }
+  using Refinements::Pathnames
+  using Refinements::Structs
+  using Versionaire::Cast
+
+  subject(:tagger) { described_class.new }
 
   include_context "with Git repository"
+  include_context "with default configuration"
 
-  using Refinements::Pathnames
-
-  let(:version) { Versionaire::Version "0.0.0" }
+  let(:configuration) { default_configuration.merge git_tag_version: Version("0.0.0") }
 
   let :tag_details do
     ->(version) { Open3.capture2(%(git show --stat --pretty=format:"%b" #{version})).first }
   end
 
-  describe "#initialize" do
-    subject(:tagger) { described_class.new }
-
-    it "answers commit prefixes" do
-      expect(tagger.commit_prefixes).to eq([])
-    end
-  end
-
-  describe "#commit_prefix_regex" do
-    context "with prefixes" do
-      it "answers regex for for commit prefixes" do
-        expect(tagger.commit_prefix_regex).to eq(/Fixed|Added|Updated|Removed/)
-      end
-    end
-
-    context "without prefixes" do
-      subject(:tagger) { described_class.new commit_prefixes: [] }
-
-      it "answers empty regex" do
-        expect(tagger.commit_prefix_regex).to eq(//)
-      end
-    end
-  end
-
-  describe "#commits" do
-    it "answers commits since last tag when tagged" do
-      git_repo_dir.change_dir do
-        `git tag 0.0.0`
-        `rm -f README.md`
-        `git commit --all --message "Removed README"`
-
-        commits = tagger.commits.map(&:subject)
-
-        expect(commits).to contain_exactly("Removed README")
-      end
-    end
-
-    it "answers all commits when not tagged" do
-      git_repo_dir.change_dir do
-        commits = tagger.commits.map(&:subject)
-        expect(commits).to contain_exactly("Added documentation")
-      end
-    end
-
-    context "with prefixed commits" do
-      before do
-        git_repo_dir.change_dir do
-          `touch a.txt && git add --all && git commit --message "This is not a good commit message"`
-          `touch b.txt && git add --all && git commit --message "Updated gem dependencies"`
-          `touch c.txt && git add --all && git commit --message "Fixed README typos"`
-          `touch d.txt && git add --all && git commit --message "Updated version release notes"`
-          `touch e.txt && git add --all && git commit --message "Removed unused stylesheets"`
-          `touch g.txt && git add --all && git commit --message "Refactored authorization"`
-        end
-      end
-
-      let :expectation do
-        [
-          "Fixed README typos",
-          "Added documentation",
-          "Updated gem dependencies",
-          "Updated version release notes",
-          "Removed unused stylesheets",
-          "Refactored authorization",
-          "This is not a good commit message"
-        ]
-      end
-
-      it "answers commits grouped by prefix and alpha-sorted per group" do
-        git_repo_dir.change_dir do
-          commits = tagger.commits.map(&:subject)
-          expect(commits).to eq(expectation)
-        end
-      end
-    end
-
-    context "with prefixed commits using special characters" do
-      subject(:tagger) { described_class.new commit_prefixes: ["[one]", "=+-#", "with spaces"] }
-
-      let :expectation do
-        [
-          "[one] more",
-          "=+-#",
-          "with spaces",
-          "Added documentation",
-          "Apple"
-        ]
-      end
-
-      before do
-        git_repo_dir.change_dir do
-          `touch a.txt && git add --all && git commit --message "Apple"`
-          `touch b.txt && git add --all && git commit --message "with spaces"`
-          `touch c.txt && git add --all && git commit --message "=+-#"`
-          `touch d.txt && git add --all && git commit --message "[one] more"`
-        end
-      end
-
-      it "answers commits grouped by prefix and alpha-sorted per group" do
-        git_repo_dir.change_dir do
-          commits = tagger.commits.map(&:subject)
-          expect(commits).to eq(expectation)
-        end
-      end
-    end
-
-    context "without prefixed commits" do
-      let(:prefixes) { [] }
-
-      before do
-        git_repo_dir.change_dir do
-          `touch a.txt && git add --all && git commit --message "One"`
-          `touch b.txt && git add --all && git commit --message "Two"`
-        end
-      end
-
-      it "answers alphabetically sorted commits" do
-        git_repo_dir.change_dir do
-          commits = tagger.commits.map(&:subject)
-          expect(commits).to eq(["Added documentation", "One", "Two"])
-        end
-      end
-    end
-
-    context "with duplicate commit messages" do
-      before do
-        git_repo_dir.change_dir do
-          `touch a.txt && git add --all && git commit --message "Updated gem dependencies"`
-          `touch b.txt && git add --all && git commit --message "Updated gem dependencies"`
-        end
-      end
-
-      it "answers commits with duplicates removed" do
-        git_repo_dir.change_dir do
-          commits = tagger.commits.map(&:subject)
-          expect(commits).to eq(["Added documentation", "Updated gem dependencies"])
-        end
-      end
-    end
-  end
-
-  describe "#commit_list" do
-    before do
-      git_repo_dir.change_dir do
-        `touch a.txt && git add --all && git commit --message "One"`
-        `touch b.txt && git add --all && git commit --message "Two"`
-      end
-    end
-
-    it "answers a formatted list of commit messages" do
-      git_repo_dir.change_dir do
-        expect(tagger.commit_list).to contain_exactly(
-          "- Added documentation - Test User",
-          "- One - Test User",
-          "- Two - Test User"
-        )
-      end
-    end
-  end
-
-  describe "#create" do
+  describe "#call" do
     it "creates tag message" do
       git_repo_dir.change_dir do
-        tagger.create version
+        tagger.call configuration
         expect(tag_details.call("0.0.0")).to match(/Version\s0\.0\.0/)
       end
     end
@@ -209,7 +52,7 @@ RSpec.describe Milestoner::Tagger do
 
       it "creates tag message with commits since last tag" do
         git_repo_dir.change_dir do
-          tagger.create version
+          tagger.call configuration
           expect(tag_details.call("0.0.0")).to match(pattern)
         end
       end
@@ -228,7 +71,7 @@ RSpec.describe Milestoner::Tagger do
         git_repo_dir.change_dir do
           `printf "Test" > README.md`
           %x(git commit --all --message 'Updated README with \`bogus command\` in message')
-          tagger.create version
+          tagger.call configuration
 
           expect(tag_details.call("0.0.0")).to match(pattern)
         end
@@ -238,65 +81,67 @@ RSpec.describe Milestoner::Tagger do
     context "when not signed" do
       it "does not sign tag" do
         git_repo_dir.change_dir do
-          tagger.create version
-          expect(tag_details.call("0.1.0")).not_to match(/-{5}BEGIN\sPGP\sSIGNATURE-{5}/)
+          tagger.call configuration
+          expect(tag_details.call("0.0.0")).not_to match(/-{5}BEGIN\sPGP\sSIGNATURE-{5}/)
         end
       end
     end
 
     context "when signed" do
       let(:gpg_dir) { temp_dir.join ".gnupg" }
-      let(:gpg_script) { temp_dir.join "..", "..", "spec", "support", "gpg_script" }
-      let :gpg_key do
-        `gpg --list-secret-keys test@example.com | grep "[A-F0-9]$" | tr -d ' '`.chomp
-      end
+      let(:gpg_script) { temp_dir.join "../../spec/support/gpg_script" }
+      let(:gpg_key) { `gpg --list-secret-keys --keyid-format SHORT | grep sec`[14..21] }
 
-      before do
+      it "signs tag" do
+        skip "Required manual input from command line."
+
         ClimateControl.modify GNUPGHOME: gpg_dir.to_s do
           git_repo_dir.change_dir do
             gpg_dir.make_dir.chmod 0o700
             `gpg --batch --generate-key --quiet --gen-key #{gpg_script}`
             `git config --local user.signingkey #{gpg_key}`
-          end
-        end
-      end
+            tagger.call configuration.merge(git_tag_sign: true)
 
-      it "signs tag" do
-        skip "Needs non-interactive support for local and remote builds" do
-          ClimateControl.modify GNUPGHOME: gpg_dir do
-            git_repo_dir.change_dir do
-              tagger.create version, sign: true
-              expect(tag_details.call("0.1.0")).to match(/-{5}BEGIN\sPGP\sSIGNATURE-{5}/)
-            end
+            expect(tag_details.call("0.0.0")).to match(/-{5}BEGIN\sPGP\sSIGNATURE-{5}/)
           end
         end
       end
     end
 
-    it "prints warning with existing tag" do
+    it "logs tag exists when previously created" do
       git_repo_dir.change_dir do
-        `git tag #{version}`
-        tagger.create version
-        result = -> { tagger.create version }
+        `git tag #{configuration.git_tag_version}`
+        result = proc { tagger.call configuration }
 
-        expect(&result).to output(/warn.+Local\stag.+0\.0\.0.+/).to_stdout
+        expect(&result).to output("Local tag exists: 0.0.0. Skipped.\n").to_stdout
       end
     end
 
-    it "doesn't print warning without existing tag" do
+    it "answers false when tag is previously created" do
       git_repo_dir.change_dir do
-        result = -> { tagger.create version }
-        expect(&result).not_to output(/warn.+Local\stag.+0\.1\.0.+/).to_stdout
+        `git tag #{configuration.git_tag_version}`
+        expect(tagger.call(configuration)).to eq(false)
       end
+    end
+
+    it "logs tag created when tag doesn't exist and is successfully created" do
+      git_repo_dir.change_dir do
+        result = proc { tagger.call configuration }
+        expect(&result).to output("Local tag created: 0.0.0.\n").to_stdout
+      end
+    end
+
+    it "answers true when tag doesn't exist and is successfully created" do
+      git_repo_dir.change_dir { expect(tagger.call(configuration)).to eq(true) }
     end
 
     context "when GPG program is missing" do
       it "signs tag" do
         git_repo_dir.change_dir do
           `git config --local gpg.program /dev/null`
-          result = -> { tagger.create version, sign: true }
+          result = -> { tagger.call configuration.merge(git_tag_sign: true) }
 
-          expect(&result).to raise_error(GitPlus::Errors::Base, "Unable to create tag: 0.0.0.")
+          expect(&result).to raise_error(Milestoner::Error, "Unable to create tag: 0.0.0.")
         end
       end
     end
@@ -304,15 +149,15 @@ RSpec.describe Milestoner::Tagger do
     it "fails with no Git commits" do
       temp_dir.change_dir do
         `git init`
-        result = proc { tagger.create version }
+        result = proc { tagger.call configuration }
 
-        expect(&result).to raise_error(Milestoner::Errors::Git, "Unable to tag without commits.")
+        expect(&result).to raise_error(Milestoner::Error, "Unable to tag without commits.")
       end
     end
 
     it "fails with invalid version" do
-      result = -> { tagger.create "bogus" }
-      expect(&result).to raise_error(Versionaire::Errors::Cast)
+      result = -> { tagger.call configuration.merge(git_tag_version: "bogus") }
+      expect(&result).to raise_error(Milestoner::Error)
     end
   end
 end
