015adbdb4b914426dc2bcd07e3d086ee7d157c2e
Brooke Kuhlmann <brooke@alchemists.io>
2021-05-02 10:49:57 -0600

    Added CLI push action
    Necessary to handle the push action of tags to a remote repository.

---
+++ lib/milestoner/cli/actions/push.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Milestoner
+  module CLI
+    module Actions
+      # Handles pushing of local tag to remote repository.
+      class Push
+        def initialize pusher: Pusher.new, container: Container
+          @pusher = pusher
+          @container = container
+        end
+
+        def call configuration
+          pusher.call configuration
+          logger.info { "Tags pushed to remote repository." }
+        end
+
+        private
+
+        attr_reader :pusher, :container
+
+        def logger = container[__method__]
+      end
+    end
+  end
+end
---
+++ spec/lib/milestoner/cli/actions/push_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Milestoner::CLI::Actions::Push do
+  subject(:action) { described_class.new pusher: pusher }
+
+  let(:pusher) { instance_spy Milestoner::Pusher }
+  let(:configuration) { Milestoner::CLI::Configuration::Content.new }
+
+  describe "#call" do
+    it "pushes tag" do
+      action.call configuration
+      expect(pusher).to have_received(:call).with(configuration)
+    end
+
+    it "logs successful push" do
+      result = proc { action.call configuration }
+      expect(&result).to output("Tags pushed to remote repository.\n").to_stdout
+    end
+
+    it "fails when unable to push" do
+      allow(pusher).to receive(:call).and_raise(Milestoner::Error, "Danger!")
+      expectation = proc { action.call configuration }
+
+      expect(&expectation).to raise_error(Milestoner::Error, "Danger!")
+    end
+  end
+end
