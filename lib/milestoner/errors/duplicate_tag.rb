01dba1428dd41102d6f6e8193ebd1f9c1f0bd78e
Brooke Kuhlmann <brooke@alchemists.io>
2021-05-10 19:58:39 -0600

    Removed errors namespace
    No longer necessary now that all objects within the system use the
    `Milestoner::Error` object instead. This has the added benefit of
    simplifying all error handling.

--- lib/milestoner/errors/duplicate_tag.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Milestoner
-  module Errors
-    # Raised for duplicate tags.
-    class DuplicateTag < Base
-    end
-  end
-end
--- spec/lib/milestoner/errors/duplicate_tag_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "spec_helper"
-
-RSpec.describe Milestoner::Errors::DuplicateTag do
-  it_behaves_like "a base error" do
-    let(:error) { described_class.new }
-  end
-end
