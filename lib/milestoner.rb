097a182352ee5b5eca3c31f72453e32ba3069512
Brooke Kuhlmann <brooke@alchemists.io>
2021-05-11 20:11:56 -0600

    Added Zeitwerk loader
    Necessary to allow Zeitwerk to manage gem requirements and reduce the
    manual management strain.
    
    [Details](https://github.com/fxn/zeitwerk).

--- lib/milestoner.rb
+++ lib/milestoner.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require "git_plus"
-require "milestoner/identity"
-require "milestoner/errors/base"
-require "milestoner/errors/duplicate_tag"
-require "milestoner/errors/git"
-require "milestoner/commit"
-require "milestoner/tagger"
-require "milestoner/pusher"
-require "milestoner/publisher"
+require "zeitwerk"
+
+loader = Zeitwerk::Loader.for_gem
+loader.inflector.inflect "cli" => "CLI"
+loader.setup
+
+# Main namespace.
+module Milestoner
+end
