11622cebf8be3f7ee5032c8bfb751dda73571bc7
Brooke Kuhlmann <brooke@alchemists.io>
2021-12-24 11:21:04 -0700

    Fixed RSpec/Dialect issues

--- spec/support/shared_examples/base_error.rb
+++ spec/support/shared_examples/base_error.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.shared_examples_for "a base error" do
+RSpec.shared_examples "a base error" do
   it "behaves like a base error" do
     expect(error).to be_a(Milestoner::Errors::Base)
   end
