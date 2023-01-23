fbec603bbdc33103e6ff4343ddbdee89750f599b
Brooke Kuhlmann <brooke@alchemists.io>
2023-01-23 14:11:35 -0700

    Refactored CLI shell act on configuration when pattern matching
    This is a minor method name consistency update since this is what Gemsmith uses when generated CLI skeletons.

--- lib/milestoner/cli/shell.rb
+++ lib/milestoner/cli/shell.rb
@@ -14,7 +14,7 @@ module Milestoner
       end
 
       def call arguments = Core::EMPTY_ARRAY
-        perform parser.call(arguments)
+        act_on parser.call(arguments)
       rescue OptionParser::ParseError, Error => error
         logger.error { error.message }
       end
@@ -23,7 +23,7 @@ module Milestoner
 
       attr_reader :parser
 
-      def perform configuration
+      def act_on configuration
         case configuration
           in action_config: Symbol => action then config.call action
           in action_publish: true then publish.call configuration
