ca38f12b005552f3b6e62a86ea3413c72f34fddf
Brooke Kuhlmann <brooke@alchemists.io>
2023-06-30 09:27:46 -0600

    Added commit part
    Necesssary to encapsulate the presentation of a commit as a line item within a milestone. This includes support for multiple output formats that will eventually be controlled via the Command Line Interface.
    
    Reek was updated to ignore multiple methods in this class since, for now, it's best to keep all of this logic together than split up the functionality further.
    
    Milestone: minor

---
+++ lib/milestoner/views/parts/commit.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+require "asciidoctor"
+require "hanami/view"
+require "refinements/array"
+
+module Milestoner
+  module Views
+    module Parts
+      # Provides presentation for an individual commit.
+      # :reek:RepeatedConditional
+      class Commit < Hanami::View::Part
+        include Import[:configuration, :sanitizer]
+
+        using Refinements::Array
+
+        def initialize(**)
+          super(**)
+          @prefixes = configuration.commit_categories.pluck :label
+          @authored_at = Time.at(value.authored_at.to_i).utc
+        end
+
+        def avatar_url(user) = format configuration.avatar_uri, id: user.external_id
+
+        def profile_url(user) = format configuration.profile_uri, id: user.handle
+
+        def kind
+          if prefixes.include? prefix then "normal"
+          elsif value.directive? then "alert"
+          else "error"
+          end
+        end
+
+        def emoji
+          configuration.commit_categories
+                       .find { |category| category.fetch(:label) == prefix }
+                       .then { |category| category ? category.fetch(:emoji) : "🔶" }
+        end
+
+        def icon
+          if prefixes.include? prefix then String(prefix).downcase
+          elsif value.directive? then "rebase"
+          else "invalid"
+          end
+        end
+
+        def safe_body = sanitizer.call(value.body).html_safe
+
+        def safe_notes = sanitizer.call(value.notes).html_safe
+
+        def total_deletions = format "%d", -value.deletions
+
+        # :reek:FeatureEnvy
+        def total_insertions
+          value.insertions.then { |total| total.positive? ? "+#{total}" : total.to_s }
+        end
+
+        def tag
+          return "rebase" if value.directive?
+          return "invalid" unless prefixes.include? prefix
+
+          value.milestone
+        end
+
+        def security = value.signature == "Good" ? "secure" : "insecure"
+
+        def at = authored_at.strftime "%Y-%m-%dT%H:%M:%S%z"
+
+        def weekday = authored_at.strftime "%A"
+
+        def date = authored_at.strftime "%Y-%m-%d"
+
+        def time = authored_at.strftime "%H:%M %p"
+
+        def zone = authored_at.strftime "%Z"
+
+        private
+
+        attr_reader :prefixes, :authored_at
+
+        def prefix = value.prefix
+      end
+    end
+  end
+end
---
+++ spec/lib/milestoner/views/parts/commit_spec.rb
@@ -0,0 +1,217 @@
+# frozen_string_literal: true
+
+require "gitt"
+require "spec_helper"
+
+RSpec.describe Milestoner::Views::Parts::Commit do
+  subject(:part) { described_class.new value: commit, rendering: view.new.rendering }
+
+  include_context "with application dependencies"
+  include_context "with enriched commit"
+  include_context "with user"
+
+  let :view do
+    Class.new Hanami::View do
+      config.paths = [Bundler.root.join("lib/milestoner/templates/releases")]
+      config.template = "n/a"
+    end
+  end
+
+  describe "#avatar_url" do
+    it "answers URL for user" do
+      expect(part.avatar_url(user)).to eq("https://avatars.githubusercontent.com/u/1")
+    end
+  end
+
+  describe "#profile_url" do
+    it "answers URL for user" do
+      expect(part.profile_url(user)).to eq("https://github.com/test")
+    end
+  end
+
+  describe "#kind" do
+    it "answers normal when valid" do
+      expect(part.kind).to eq("normal")
+    end
+
+    context "when subject has directive" do
+      let(:commit) { Milestoner::Models::Commit[subject: "fixup! Added documentation"] }
+
+      it "answers alert" do
+        expect(part.kind).to eq("alert")
+      end
+    end
+
+    context "when subject is unknown" do
+      let(:commit) { Milestoner::Models::Commit[subject: "Created documentation"] }
+
+      it "answers error" do
+        expect(part.kind).to eq("error")
+      end
+    end
+
+    context "when subject has no prefix" do
+      let(:commit) { Milestoner::Models::Commit[subject: "----- Breakpoint -----"] }
+
+      it "answers error" do
+        expect(part.kind).to eq("error")
+      end
+    end
+  end
+
+  describe "#emoji" do
+    it "answers emoji" do
+      expect(part.emoji).to eq("🟢")
+    end
+
+    context "when subject has directive" do
+      let(:commit) { Milestoner::Models::Commit[subject: "fixup! Added documentation"] }
+
+      it "answers alert emoji" do
+        expect(part.emoji).to eq("🔶")
+      end
+    end
+
+    context "when subject is invalid" do
+      let(:commit) { Milestoner::Models::Commit[subject: "Created documentation"] }
+
+      it "answers alert emoji" do
+        expect(part.emoji).to eq("🔶")
+      end
+    end
+  end
+
+  describe "#icon" do
+    it "answers specific icon for known subject" do
+      expect(part.icon).to eq("added")
+    end
+
+    context "when subject has directive" do
+      let(:commit) { Milestoner::Models::Commit[subject: "fixup! Added documentation"] }
+
+      it "answers rebase icon" do
+        expect(part.icon).to eq("rebase")
+      end
+    end
+
+    context "when subject is invalid" do
+      let(:commit) { Milestoner::Models::Commit[subject: "Created documentation"] }
+
+      it "answers invalid icon" do
+        expect(part.icon).to eq("invalid")
+      end
+    end
+  end
+
+  describe "#total_deletions" do
+    it "answers total" do
+      expect(part.total_deletions).to eq("-10")
+    end
+  end
+
+  describe "#total_insertions" do
+    it "answers total" do
+      expect(part.total_insertions).to eq("+5")
+    end
+
+    context "when zero" do
+      let(:commit) { Milestoner::Models::Commit[insertions: 0] }
+
+      it "answers zero" do
+        expect(part.total_insertions).to eq("0")
+      end
+    end
+  end
+
+  describe "#tag" do
+    it "answers version when subject is valid" do
+      expect(part.tag).to eq("patch")
+    end
+
+    context "with prefix directive" do
+      let(:commit) { Milestoner::Models::Commit[subject: "fixup! Added test"] }
+
+      it "answers rebase" do
+        expect(part.tag).to eq("rebase")
+      end
+    end
+
+    context "with invalid prefix" do
+      let(:commit) { Milestoner::Models::Commit[subject: "A test"] }
+
+      it "answers invalid" do
+        expect(part.tag).to eq("invalid")
+      end
+    end
+  end
+
+  describe "#safe_body" do
+    let :commit do
+      Milestoner::Models::Commit[
+        subject: "Test",
+        body: %(<a href="https://test.com">Test</a><script>Danger!</script>)
+      ]
+    end
+
+    it "answers safe body" do
+      expect(part.safe_body).to eq(%(<a href="https://test.com" rel="nofollow">Test</a>))
+    end
+  end
+
+  describe "#safe_notes" do
+    let :commit do
+      Milestoner::Models::Commit[
+        subject: "Test",
+        notes: %(<a href="https://test.com">Test</a><script>Danger!</script>)
+      ]
+    end
+
+    it "answers safe notes" do
+      expect(part.safe_notes).to eq(%(<a href="https://test.com" rel="nofollow">Test</a>))
+    end
+  end
+
+  describe "#security" do
+    it "answers secure with valid signature" do
+      expect(part.security).to eq("secure")
+    end
+
+    context "with invalid signature" do
+      let(:commit) { Milestoner::Models::Commit[signature: "Bad"] }
+
+      it "answers insecure" do
+        expect(part.security).to eq("insecure")
+      end
+    end
+  end
+
+  describe "#at" do
+    it "answers machine readable date and time" do
+      expect(part.at).to eq("2023-01-01T17:10:10+0000")
+    end
+  end
+
+  describe "#weekday" do
+    it "answers weekday" do
+      expect(part.weekday).to eq("Sunday")
+    end
+  end
+
+  describe "#date" do
+    it "answers date" do
+      expect(part.date).to eq("2023-01-01")
+    end
+  end
+
+  describe "#time" do
+    it "answers time" do
+      expect(part.time).to eq("17:10 PM")
+    end
+  end
+
+  describe "#zone" do
+    it "answers timezone" do
+      expect(part.zone).to eq("UTC")
+    end
+  end
+end
