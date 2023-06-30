f5324457f39fc77d48cb634597d885e6cbdf7e17
Brooke Kuhlmann <brooke@alchemists.io>
2023-06-30 09:25:08 -0600

    Added view context
    Necessary to provide a common information related to all views based on configuration information as input from the CLI.
    
    Milestone: minor

---
+++ lib/milestoner/views/context.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "forwardable"
+require "hanami/view"
+
+module Milestoner
+  module Views
+    # Provides a common context for all views.
+    class Context < Hanami::View::Context
+      extend Forwardable
+
+      include Import[:input]
+
+      delegate %i[
+        project_author
+        project_description
+        project_generator
+        project_label
+        project_version
+      ] => :input
+
+      def project_title = [project_label, project_version].compact.join " "
+    end
+  end
+end
---
+++ spec/lib/milestoner/views/context_spec.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "versionaire"
+
+RSpec.describe Milestoner::Views::Context do
+  using Refinements::Struct
+  using Versionaire::Cast
+
+  subject(:a_context) { described_class.new }
+
+  include_context "with application dependencies"
+
+  describe "#project_author" do
+    it "answers configuration author" do
+      expect(a_context.project_author).to eq("Test Author")
+    end
+  end
+
+  describe "#project_description" do
+    it "answers configuration description" do
+      expect(a_context.project_description).to eq("Test description.")
+    end
+  end
+
+  describe "#project_generator" do
+    it "answers configuration generator" do
+      expect(a_context.project_generator).to eq("Test Generator")
+    end
+  end
+
+  describe "#project_label" do
+    it "answers configuration label" do
+      expect(a_context.project_label).to eq("Test Label")
+    end
+  end
+
+  describe "#project_title" do
+    it "answers labeled version" do
+      expect(a_context.project_title).to eq("Test Label 1.2.3")
+    end
+
+    it "answers label only when version is missing" do
+      input.project_version = nil
+      expect(a_context.project_title).to eq("Test Label")
+    end
+
+    it "answers version only when label is missing" do
+      input.project_label = nil
+      expect(a_context.project_title).to eq("1.2.3")
+    end
+
+    it "answers empty string when label and version are missing" do
+      input.project_label = nil
+      input.project_version = nil
+
+      expect(a_context.project_title).to eq("")
+    end
+  end
+
+  describe "#project_version" do
+    it "answers configuration version" do
+      expect(a_context.project_version).to eq(Version("1.2.3"))
+    end
+  end
+end
