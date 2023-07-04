bbbb2f75d296ce431efcabb48f9d199d169b859c
Brooke Kuhlmann <brooke@alchemists.io>
2023-07-04 13:17:49 -0600

    Added CLI build label action
    Necessary to specify or dynamically compute the label when building a milestone.
    
    The Reek configuration was updated to ignore the control parameter since use of a default is currently a bug with the Option Parser gem.
    
    Milestone: minor

---
+++ lib/milestoner/cli/actions/build/label.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "pathname"
+require "sod"
+require "spek"
+
+module Milestoner
+  module CLI
+    module Actions
+      module Build
+        # Handles build label.
+        class Label < Sod::Action
+          include Import[:input]
+
+          description "Set label."
+
+          on %w[-l --label], argument: "[TEXT]"
+
+          default { Spek::Loader.call("#{Pathname.pwd.basename}.gemspec").label }
+
+          def call(label = nil) = input.project_label = label || default
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/milestoner/cli/actions/build/label_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Milestoner::CLI::Actions::Build::Label do
+  subject(:action) { described_class.new }
+
+  include_context "with application dependencies"
+
+  describe "#call" do
+    it "answers default label" do
+      action.call
+      expect(input.project_label).to match(/Milestoner|Undefined/)
+    end
+
+    it "answers custom label" do
+      action.call "test"
+      expect(input.project_label).to eq("test")
+    end
+  end
+end
