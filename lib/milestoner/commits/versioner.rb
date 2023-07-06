d5b45118d467f1e9d8310b752468de567889ffdd
Brooke Kuhlmann <brooke@alchemists.io>
2023-07-06 16:51:07 -0600

    Added commits versioner
    Necessary to analyze unversioned commits, last tag, and determine what the next version should be. This will be used to handle the automatic updating of a version.
    
    Reek was updated to ignore the nested iterator for trailer milestones since breaking this down further wouldn't be of much help so keeping this together instead.
    
    Milestone: minor

---
+++ lib/milestoner/commits/versioner.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "dry/monads"
+require "versionaire"
+
+module Milestoner
+  module Commits
+    # Calculates next version based on commit trailer version keys.
+    class Versioner
+      include Import[:git]
+      include Dry::Monads[:result]
+
+      using Versionaire::Cast
+
+      DEFAULTS = {trailer_key: "Milestone", fallback: Versionaire::Version.new}.freeze
+
+      def initialize(defaults: DEFAULTS, collector: Collector.new, **)
+        @defaults = defaults
+        @collector = collector
+        super(**)
+      end
+
+      def call
+        trailer_milestones.then { |milestones| bump milestones }
+                          .value_or(fallback)
+      end
+
+      private
+
+      attr_reader :defaults, :collector
+
+      def trailer_milestones
+        collector.call.value_or([]).each.with_object [] do |commit, values|
+          commit.trailer_value_for(trailer_key).bind { |milestone| values.append milestone.to_sym }
+        end
+      end
+
+      def bump milestones
+        target = fallback.members.intersection(milestones).first
+        git.tag_last.fmap { |tag| target ? Version(tag).bump(target) : fallback }
+      end
+
+      def trailer_key = defaults.fetch __method__
+
+      def fallback = defaults.fetch __method__
+    end
+  end
+end
---
+++ spec/lib/milestoner/commits/versioner_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Milestoner::Commits::Versioner do
+  using Refinements::Pathname
+  using Versionaire::Cast
+
+  subject(:versioner) { described_class.new }
+
+  include_context "with Git repository"
+
+  describe "#call" do
+    it "answers next major version" do
+      git_repo_dir.change_dir do
+        `git tag 0.0.0`
+        `touch a.txt && git add --all && git commit --message "Added A" --trailer "Milestone=patch"`
+        `touch b.txt && git add --all && git commit --message "Added B" --trailer "Milestone=major"`
+        `touch c.txt && git add --all && git commit --message "Added C" --trailer "Milestone=minor"`
+
+        expect(versioner.call).to eq(Version("1.0.0"))
+      end
+    end
+
+    it "answers next minor version" do
+      git_repo_dir.change_dir do
+        `git tag 0.0.0`
+        `touch a.txt && git add --all && git commit --message "Added A" --trailer "Milestone=patch"`
+        `touch b.txt && git add --all && git commit --message "Added B" --trailer "Milestone=patch"`
+        `touch c.txt && git add --all && git commit --message "Added C" --trailer "Milestone=minor"`
+
+        expect(versioner.call).to eq(Version("0.1.0"))
+      end
+    end
+
+    it "answers next patch version" do
+      git_repo_dir.change_dir do
+        `git tag 0.0.0`
+        `touch a.txt && git add --all && git commit --message "Added A" --trailer "Milestone=patch"`
+        `touch b.txt && git add --all && git commit --message "Added B" --trailer "Milestone=patch"`
+        `touch c.txt && git add --all && git commit --message "Added C" --trailer "Milestone=patch"`
+
+        expect(versioner.call).to eq(Version("0.0.1"))
+      end
+    end
+
+    it "answers default version without version trailers" do
+      git_repo_dir.change_dir do
+        `git tag 0.0.0`
+        `touch a.txt && git add --all && git commit --message "Added A"`
+        `touch b.txt && git add --all && git commit --message "Added B"`
+        `touch c.txt && git add --all && git commit --message "Added C"`
+
+        expect(versioner.call).to eq(Version("0.0.0"))
+      end
+    end
+
+    it "answers default version with versioned commits and no tags" do
+      git_repo_dir.change_dir do
+        `touch a.txt && git add --all && git commit --message "Added A" --trailer "Milestone=major"`
+        expect(versioner.call).to eq(Version("0.0.0"))
+      end
+    end
+
+    it "answers default version when no tags exist" do
+      git_repo_dir.change_dir { expect(versioner.call).to eq(Version("0.0.0")) }
+    end
+  end
+end
