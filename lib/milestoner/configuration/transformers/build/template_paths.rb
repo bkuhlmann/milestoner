65c35156b0e5710f6bfe9a5a744ce3fbc57e8b3d
Brooke Kuhlmann <brooke@alchemists.io>
2023-07-12 09:47:27 -0600

    Added build template paths configuration transformer
    Necessary to assemble and assign the template paths used by this gem for the Hanami views based on an XDG configuration. If no XDG configuration is found, then the templates provided by this gem will be used instead. This information will soon be used for build purposes and by all views.
    
    Milestone: minor

---
+++ lib/milestoner/configuration/transformers/build/template_paths.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "dry/monads"
+require "pathname"
+require "runcom"
+
+module Milestoner
+  module Configuration
+    module Transformers
+      module Build
+        # Ensures XDG configuration and gem template paths are configured.
+        class TemplatePaths
+          include Dry::Monads[:result]
+
+          def initialize key = :build_template_paths,
+                         default: Pathname(__dir__).join("../../../templates"),
+                         xdg_config: Runcom::Config.new("milestoner/templates")
+            @key = key
+            @default = default
+            @xdg_config = xdg_config
+          end
+
+          def call content
+            content[key] = xdg_config.all.append default
+            Success content
+          end
+
+          private
+
+          attr_reader :key, :default, :xdg_config
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/milestoner/configuration/transformers/build/template_paths_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Milestoner::Configuration::Transformers::Build::TemplatePaths do
+  include Dry::Monads[:result]
+
+  using Refinements::Pathname
+
+  subject(:transformer) { described_class.new }
+
+  describe "#call" do
+    it "answers XDG configuration paths plus gem templates path" do
+      expect(transformer.call({})).to eq(
+        Success(
+          build_template_paths: [
+            Bundler.root.join(".config/milestoner/templates"),
+            Pathname("~/.config/milestoner/templates").expand_path,
+            Pathname("/etc/xdg/milestoner/templates"),
+            Bundler.root.join("lib/milestoner/templates")
+          ]
+        )
+      )
+    end
+  end
+end
