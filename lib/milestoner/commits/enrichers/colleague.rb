894562d0a9823d201d3cbe20e95a0100fb911749
Brooke Kuhlmann <brooke@alchemists.io>
2023-07-20 16:18:24 -0600

    Added commit colleague enricher
    Necessary to resolve a user from the cache (if found) based on trailer information. Can be used for multiple trailer keys like `co-authored-by`, `signed-by`, etc.
    
    Milestone: minor

---
+++ lib/milestoner/commits/enrichers/colleague.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "gitt"
+
+module Milestoner
+  module Commits
+    module Enrichers
+      # Enriches a commit colleague by using cache.
+      class Colleague
+        include Milestoner::Import[:cache]
+
+        def initialize(key:, parser: Gitt::Parsers::Person.new, **)
+          @key = key
+          @parser = parser
+          super(**)
+        end
+
+        def call(commit) = commit.find_trailers(key).bind { |trailers| users_for(trailers).compact }
+
+        private
+
+        attr_reader :key, :parser
+
+        def users_for(trailers) = trailers.map { |trailer| user_for parser.call(trailer.value) }
+
+        def user_for person
+          cache.commit(:users) { find person.name }
+               .value_or(nil)
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/milestoner/commits/enrichers/colleague_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Milestoner::Commits::Enrichers::Colleague do
+  subject(:enricher) { described_class.new key: "co-authored-by" }
+
+  include_context "with application dependencies"
+  include_context "with user"
+
+  describe "#call" do
+    it "answers users when found in cache" do
+      record = user
+      cache.commit(:users) { upsert record }
+
+      commit = Gitt::Models::Commit[
+        trailers: [
+          Gitt::Models::Trailer[key: "co-authored-by", value: "Test <test@example.com"]
+        ]
+      ]
+
+      expect(enricher.call(commit)).to eq([user])
+    end
+
+    it "answers empty array when user isn't found in cache" do
+      commit = Gitt::Models::Commit[
+        trailers: [
+          Gitt::Models::Trailer[key: "co-authored-by", value: "Test <test@example.com"]
+        ]
+      ]
+
+      expect(enricher.call(commit)).to eq([])
+    end
+
+    it "answers empty array when trailers are empty" do
+      commit = Gitt::Models::Commit[trailers: []]
+      expect(enricher.call(commit)).to eq([])
+    end
+  end
+end
