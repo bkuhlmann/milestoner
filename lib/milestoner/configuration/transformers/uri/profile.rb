d481d85b1d197f081ed5e9e9179ef68024d92238
Brooke Kuhlmann <brooke@alchemists.io>
2024-01-04 19:25:22 -0700

    Added profile URI configuration transformer
    Necessary to transform profile URIs into full links to team member profiles.
    
    Milestone: minor

---
+++ lib/milestoner/configuration/transformers/uri/profile.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "dry/monads"
+require "pathname"
+
+module Milestoner
+  module Configuration
+    module Transformers
+      module URI
+        Profile = lambda do |content, key = :profile_uri|
+          domain, uri = content.values_at :profile_domain, key
+
+          return Dry::Monads::Success content unless uri
+
+          content[key] = format uri, domain:, id: "%<id>s"
+          Dry::Monads::Success content
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/milestoner/configuration/transformers/uri/profile_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Milestoner::Configuration::Transformers::URI::Profile do
+  include Dry::Monads[:result]
+
+  subject(:transformer) { described_class }
+
+  describe "#call" do
+    describe "#call" do
+      it "answers formatted URI when URI exists" do
+        content = {profile_domain: "https://example.com", profile_uri: "%<domain>s/%<id>s"}
+
+        expect(transformer.call(content)).to eq(
+          Success(
+            profile_domain: "https://example.com",
+            profile_uri: "https://example.com/%<id>s"
+          )
+        )
+      end
+
+      it "answers original content when URI doesn't exist" do
+        expect(transformer.call({})).to eq(Success({}))
+      end
+    end
+  end
+end
