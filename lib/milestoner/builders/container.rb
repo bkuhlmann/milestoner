dcfd9f06bbbfef77d55ef7659d45ffe6e1754e10
Brooke Kuhlmann <brooke@alchemists.io>
2024-01-14 07:14:37 -0700

    Added ASCII Doc and Markdown builders to container
    Necessary for future injection.
    
    Milestone: minor

--- lib/milestoner/builders/container.rb
+++ lib/milestoner/builders/container.rb
@@ -8,6 +8,8 @@ module Milestoner
     module Container
       extend Dry::Container::Mixin
 
+      register(:ascii_doc, memoize: true) { ASCIIDoc.new }
+      register(:markdown, memoize: true) { Markdown.new }
       register(:stream, memoize: true) { Stream.new }
       register(:web, memoize: true) { Web.new }
     end
