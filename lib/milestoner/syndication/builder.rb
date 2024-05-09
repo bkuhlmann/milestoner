d43f9259bfa316fa6199b9da3252942ab780a6c8
Brooke Kuhlmann <brooke@alchemists.io>
2024-05-09 08:57:47 -0600

    Added syndication builder
    Provides low-level support for building feeds (RSS/Atom). This will soon be used by the CLI builders for use via the CLI so syndication can be another output format.
    
    Milestone: minor

---
+++ lib/milestoner/syndication/builder.rb
@@ -0,0 +1,120 @@
+# frozen_string_literal: true
+
+require "core"
+require "dry/monads"
+require "rss"
+
+module Milestoner
+  module Syndication
+    # Builds feed in Atom format.
+    # :reek:DataClump
+    class Builder
+      include Import[:settings]
+      include Dry::Monads[:result]
+
+      using Refine
+
+      def initialize(client: RSS::Maker, view: Views::Milestones::Show.new, **)
+        super(**)
+        @client = client
+        @view = view
+      end
+
+      def call tags
+        return Failure "No content." if tags.empty?
+
+        Success build_feed(tags).to_s
+      rescue NoMethodError, RSS::Error => error
+        Failure "#{self.class}: #{error.message.capitalize}."
+      end
+
+      private
+
+      attr_reader :client, :view
+
+      def build_feed tags
+        client.make "atom" do |node|
+          build_channel node.channel, tags
+          build_tags node, tags
+        end
+      end
+
+      def build_channel node, tags
+        at = tags.first.committed_at
+
+        node.merge id: settings.project_uri_home,
+                   title: settings.syndication_label,
+                   subtitle: settings.project_description,
+                   icon: settings.project_uri_icon,
+                   logo: settings.project_uri_logo,
+                   rights: at.strftime("%Y"),
+                   updated: at
+
+        build_channel_elements node, tags
+      end
+
+      def build_channel_elements node, tags
+        build_links node
+        build_generator node
+
+        build_authors node,
+                      tags.flat_map { |tag| tag.commits.map(&:author) }
+                          .uniq
+
+        node.categories.build label: settings.project_label, term: settings.project_name
+      end
+
+      def build_links node
+        node.links.build_for settings.syndication_links,
+                             label: :title,
+                             uri: :href,
+                             relation: :rel,
+                             mime: :type
+      end
+
+      def build_generator node
+        node.generator do |generator|
+          generator.merge content: settings.generator_label,
+                          version: settings.generator_version,
+                          uri: settings.generator_uri
+        end
+      end
+
+      def build_tags node, tags = Core::EMPTY_ARRAY
+        tags.each { |tag| build_item node.items, tag }
+      end
+
+      def build_item node, tag
+        node.new_item do |item|
+          build_item_metadata item, tag
+          build_item_content item.content, tag
+          build_authors item, tag.commits.map(&:author).uniq.select(&:name)
+          item.categories.build_for settings.syndication_categories, label: :label, name: :term
+        end
+      end
+
+      def build_item_metadata node, tag
+        committed_at = tag.committed_at
+        version = tag.version
+
+        node.merge id: format(settings.syndication_id, id: version),
+                   title: format(settings.syndication_entry_label, id: version),
+                   link: format(settings.syndication_entry_uri, id: version),
+                   rights: committed_at.strftime("%Y"),
+                   published: committed_at,
+                   updated: committed_at
+      end
+
+      def build_item_content node, tag
+        content = view.call tag:, layout: settings.build_layout, format: :xml
+        node.merge content:, type: :html
+      end
+
+      def build_authors node, users = Core::EMPTY_ARRAY
+        users.each do |user|
+          node.authors.build name: user.name, uri: format(settings.profile_uri, id: user.handle)
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/milestoner/syndication/builder_spec.rb
@@ -0,0 +1,183 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Milestoner::Syndication::Builder do
+  include Dry::Monads[:result]
+
+  using Refinements::Struct
+
+  subject(:builder) { described_class.new }
+
+  include_context "with application dependencies"
+  include_context "with enriched tag"
+
+  describe "#call" do
+    let(:feed) { builder.call(tags).bind { |body| RSS::Parser.parse body } }
+    let(:authors) { [Milestoner::Models::User[external_id: 1, handle: "test", name: "Test User"]] }
+    let(:at) { Time.utc 2024, 7, 5, 1, 1, 1 }
+
+    it "answers feed author name" do
+      expect(feed.author.name.content).to eq("Zoe Washburne")
+    end
+
+    it "answers feed author uri" do
+      expect(feed.author.uri.content).to eq("https://github.com/zoe")
+    end
+
+    it "answers feed category term" do
+      expect(feed.category.term).to eq("test")
+    end
+
+    it "answers feed category label" do
+      expect(feed.category.label).to eq("Test")
+    end
+
+    it "answers feed generator URI" do
+      expect(feed.generator.uri).to eq("https://alchemists.io/projects/milestoner")
+    end
+
+    it "answers feed generator label" do
+      expect(feed.generator.version).to eq("3.2.1")
+    end
+
+    it "answers feed icon" do
+      expect(feed.icon.content).to eq(
+        "https://undefined.io/assets/media/projects/test/logo.png"
+      )
+    end
+
+    it "answers feed ID" do
+      expect(feed.id.content).to eq("https://undefined.io/projects/milestoner")
+    end
+
+    it "answers feed links" do
+      links = feed.links.reduce [] do |all, link|
+        all << {href: link.href, rel: link.rel, type: link.type, title: link.title}
+      end
+
+      expect(links).to eq(
+        [
+          {
+            href: "https://undefined.io/projects/test/versions",
+            rel: "alternate",
+            type: "text/html",
+            title: "Undefined: Milestoner (web)"
+          },
+          {
+            href: "https://undefined.io/projects/test/versions.xml",
+            rel: "self",
+            type: "application/atom+xml",
+            title: "Undefined: Milestoner (feed)"
+          }
+        ]
+      )
+    end
+
+    it "answers feed logo" do
+      expect(feed.logo.content).to eq(
+        "https://undefined.io/assets/media/projects/test/logo.png"
+      )
+    end
+
+    it "answers feed rights" do
+      expect(feed.rights.content).to eq("2024")
+    end
+
+    it "answers feed subtitle" do
+      expect(feed.subtitle.content).to eq("A test.")
+    end
+
+    it "answers feed title" do
+      expect(feed.title.content).to eq("Undefined: Milestoner")
+    end
+
+    it "answers feed updated" do
+      expect(feed.updated.content).to eq(at)
+    end
+
+    it "answers feed DC date" do
+      expect(feed.dc_date).to eq(at)
+    end
+
+    it "answers entry id" do
+      expect(feed.entries.first.id.content).to eq(
+        "https://undefined.io/projects/test/versions/0.1.0"
+      )
+    end
+
+    it "answers entry title" do
+      expect(feed.entries.first.title.content).to eq("0.1.0")
+    end
+
+    it "answers entry link" do
+      expect(feed.entries.first.link.href).to eq(
+        "https://undefined.io/projects/test/versions/0.1.0"
+      )
+    end
+
+    it "answers entry rights" do
+      expect(feed.entries.first.rights.content).to eq("2024")
+    end
+
+    it "answers entry published" do
+      expect(feed.entries.first.published.content).to eq(at)
+    end
+
+    it "answers entry updated" do
+      expect(feed.entries.first.updated.content).to eq(at)
+    end
+
+    it "answers entry dc date" do
+      expect(feed.entries.first.dc_date).to eq(at)
+    end
+
+    it "answers entry content" do
+      expect(feed.entries.first.content.content).to include("Added documentation")
+    end
+
+    it "answers entry content type" do
+      expect(feed.entries.first.content.type).to include("html")
+    end
+
+    it "answers entry authors" do
+      content = feed.entries.first.authors.reduce(+"") { |body, item| body.concat item.to_s }
+
+      expect(content).to eq(<<~CONTENT.strip)
+        <author>
+          <name>Zoe Washburne</name>
+          <uri>https://github.com/zoe</uri>
+        </author>
+      CONTENT
+    end
+
+    it "answers entry categories" do
+      content = feed.entries.first.categories.reduce(+"") { |body, item| body.concat item.to_s }
+
+      expect(content).to eq(<<~CONTENT.strip)
+        <category term="milestones"
+          label="Milestones"/>
+      CONTENT
+    end
+
+    it "answers failure when authors are missing" do
+      tag.commits.clear
+
+      expect(builder.call([tag])).to match(
+        Failure("#{described_class}: Required variables of maker.channel are not set: author.")
+      )
+    end
+
+    it "answers failure without milestones" do
+      expect(builder.call([])).to eq(Failure("No content."))
+    end
+
+    it "answers failure when attribute is missing" do
+      tag.committed_at = nil
+
+      expect(builder.call([tag])).to match(
+        Failure("#{described_class}: Undefined method `strftime' for nil.")
+      )
+    end
+  end
+end
