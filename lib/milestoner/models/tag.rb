15ca5cf0cfe0e462ada2e77d51b98607b8b1310a
Brooke Kuhlmann <brooke@alchemists.io>
2024-05-23 10:53:01 -0600

    Added tag model
    Necessary to represent a Git tag. The attributes breakdown as follows:
    
    * `author`: The author (user) who created the tag.
    * `commits`: The array of commits which make up the milestone.
    * `committed_at`: The time at which the tag was created.
    * `sha`: The commit SHA for which the tag is associated with.
    * `signature`: The public signature (if any) used to sign the tag.
    * `version`: The milestone version.
    
    Milestone: minor

---
+++ lib/milestoner/models/tag.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Milestoner
+  module Models
+    # Represents a Git tag comprised of multiple commits.
+    Tag = Struct.new :author, :commits, :committed_at, :sha, :signature, :version
+  end
+end
