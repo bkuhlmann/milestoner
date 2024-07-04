bcf73601c7c01379acac65669fbd8bbcbada7ba8
Brooke Kuhlmann <brooke@alchemists.io>
2024-07-04 14:27:35 -0600

    Refactored show templates to use tag partials
    Necessary to reduce duplication and delegate to the tag partial which handles the rendering of tag information now.
    
    Milestone: patch

--- lib/milestoner/views/milestones/show.rb
+++ lib/milestoner/views/milestones/show.rb
@@ -1,48 +1,20 @@
 # frozen_string_literal: true
 
 require "hanami/view"
-require "refinements/string"
 
 module Milestoner
   module Views
     module Milestones
-      # Produces release notes in HTML format.
+      # Renders release notes.
       class Show < Hanami::View
         include Import[:settings]
-
-        using Refinements::String
 
         config.default_context = Context.new
         config.part_namespace = Parts
         config.paths = Container[:settings].build_template_paths
         config.template = "milestones/show"
 
-        expose :commits
-        expose(:project_uri_home) { settings.project_uri_home }
-        expose(:project_uri_logo) { settings.project_uri_logo }
-        expose :at, default: Time.now.utc
-        expose(:date) { |at| at.strftime "%Y-%m-%d" }
-        expose(:datetime) { |at| at.strftime "%Y-%m-%dT%H:%M:%S%z" }
-
-        expose :total_commits do |commits|
-          total = commits.size
-          "#{total} commit".pluralize "s", total
-        end
-
-        expose :total_files do |commits|
-          total = commits.sum(&:files_changed)
-          "#{total} file".pluralize "s", total
-        end
-
-        expose :total_deletions do |commits|
-          total = commits.sum(&:deletions)
-          "#{total} deletion".pluralize "s", total
-        end
-
-        expose :total_insertions do |commits|
-          total = commits.sum(&:insertions)
-          "#{total} insertion".pluralize "s", total
-        end
+        expose :tag
       end
     end
   end
--- spec/lib/milestoner/views/milestones/show_spec.rb
+++ spec/lib/milestoner/views/milestones/show_spec.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require "gitt"
 require "spec_helper"
 
 RSpec.describe Milestoner::Views::Milestones::Show do
@@ -10,68 +9,23 @@ RSpec.describe Milestoner::Views::Milestones::Show do
   subject(:view) { described_class.new }
 
   include_context "with application dependencies"
-  include_context "with enriched commit"
+  include_context "with enriched tag"
 
   describe "#call" do
-    it "answers no activity when there are no commits" do
-      expect(view.call(commits: []).to_s).to include(%(<p class="dormant"></p>))
-    end
-
     it "includes label" do
-      expect(view.call(commits: [commit]).to_s).to include("Test")
+      expect(view.call(tag:).to_s).to include("Test")
     end
 
     it "includes version" do
-      expect(view.call(commits: [commit]).to_s).to match(/\d+\.\d+\.\d+/)
+      expect(view.call(tag:).to_s).to match(/\d+\.\d+\.\d+/)
     end
 
     it "includes URI" do
-      expect(view.call(commits: [commit]).to_s).to include("/projects/milestoner")
-    end
-
-    it "includes date" do
-      expect(view.call(commits: [], at: Time.parse("2020-01-10")).to_s).to include("2020-01-10")
-    end
-
-    it "includes datetime" do
-      at = Time.utc 2020, 1, 10, 10, 10, 10
-      expect(view.call(commits: [], at:).to_s).to include("2020-01-10T10:10:10+0000")
+      expect(view.call(tag:).to_s).to include("/projects/milestoner")
     end
 
     it "includes commit" do
-      expect(view.call(commits: [commit]).to_s).to include("Added documentation")
-    end
-
-    it "includes total commits (singular)" do
-      expect(view.call(commits: [commit]).to_s).to include("1 commit.")
-    end
-
-    it "includes total commits (plural)" do
-      expect(view.call(commits: [commit, commit]).to_s).to include("2 commits.")
-    end
-
-    it "includes total files (singular)" do
-      expect(view.call(commits: [commit.merge(files_changed: 1)]).to_s).to include("1 file.")
-    end
-
-    it "includes total files (plural)" do
-      expect(view.call(commits: [commit]).to_s).to include("2 files.")
-    end
-
-    it "answers total deletions (singular)" do
-      expect(view.call(commits: [commit.merge(deletions: 1)]).to_s).to include("1 deletion.")
-    end
-
-    it "answers total deletions (plural)" do
-      expect(view.call(commits: [commit]).to_s).to include("10 deletions.")
-    end
-
-    it "answers total insertions (singular)" do
-      expect(view.call(commits: [commit.merge(insertions: 1)]).to_s).to include("1 insertion.")
-    end
-
-    it "answers total insertions (plural)" do
-      expect(view.call(commits: [commit]).to_s).to include("5 insertions.")
+      expect(view.call(tag:).to_s).to include("Added documentation")
     end
   end
 end
