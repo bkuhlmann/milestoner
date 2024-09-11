4aca9f30fe27c897ea3307763aa9077d228b85c3
Brooke Kuhlmann <brooke@alchemists.io>
2024-09-11 15:03:00 -0600

    Added Markdown renderer syntax highlighting
    As done with ASCII Doc, this leverages link:https://rouge.jneen.net[Rouge] to apply syntax highlighting to Markdown content so ASCII Doc and Markdown formats have consistent behavior. This required extracting the client to a constant for the following reasons:
    
    * Provides an anonymous client class as an HTML renderer with Rouge behavior. The temporary name is used to give the subclass a better name for readability and debugging purposes. I did this because, at the moment, there isn't a strong need to reuse this class. This client is only meant to serve this renderer.
    * While providing a customized client, I added additional support for code blocks, footnotes, highlights, superscript, and tables in an effort to provide some parity with what you get when using the ASCII Doc format.
    
    Milestone: minor

--- lib/milestoner/renderers/markdown.rb
+++ lib/milestoner/renderers/markdown.rb
@@ -1,12 +1,25 @@
 # frozen_string_literal: true
 
 require "redcarpet"
+require "rouge"
+require "rouge/plugins/redcarpet"
 
 module Milestoner
   module Renderers
     # Renders Markdown as HTML.
     class Markdown
-      def initialize client: Redcarpet::Markdown.new(Redcarpet::Render::HTML.new)
+      CLIENT = Redcarpet::Markdown.new Class.new(Redcarpet::Render::HTML)
+                                            .include(Rouge::Plugins::Redcarpet)
+                                            .set_temporary_name("redcarpet_html_rouge")
+                                            .new,
+                                       disable_indented_code_blocks: true,
+                                       fenced_code_blocks: true,
+                                       footnotes: true,
+                                       highlight: true,
+                                       superscript: true,
+                                       tables: true
+
+      def initialize client: CLIENT
         @client = client
       end
 
--- spec/lib/milestoner/renderers/markdown_spec.rb
+++ spec/lib/milestoner/renderers/markdown_spec.rb
@@ -29,5 +29,73 @@ RSpec.describe Milestoner::Renderers::Markdown do
         </ul>
       PROOF
     end
+
+    it "answers HTML code block" do
+      html = renderer.call <<~CONTENT
+        ``` ruby
+        1 + 1 = 2
+        ```
+      CONTENT
+
+      expect(html).to eq(<<~PROOF.strip)
+        <div class="highlight"><pre class="highlight ruby"><code><span class="mi">1</span> <span class="o">+</span> <span class="mi">1</span> <span class="o">=</span> <span class="mi">2</span>
+        </code></pre></div>
+      PROOF
+    end
+
+    it "answers HTML indented code block as a paragraph" do
+      html = renderer.call "    1 + 1 = 2"
+      expect(html).to eq("<p>1 + 1 = 2</p>\n")
+    end
+
+    it "answers HTML footnotes" do
+      html = renderer.call <<~CONTENT
+        This is a test.[^1]
+
+        [^1]: A footnote.
+      CONTENT
+
+      expect(html).to eq(<<~PROOF)
+        <p>This is a test.<sup id="fnref1"><a href="#fn1">1</a></sup></p>
+
+        <div class="footnotes">
+        <hr>
+        <ol>
+
+        <li id="fn1">
+        <p>A footnote.&nbsp;<a href="#fnref1">&#8617;</a></p>
+        </li>
+
+        </ol>
+        </div>
+      PROOF
+    end
+
+    it "answers HTML highlight" do
+      html = renderer.call "This is a ==highlight==."
+      expect(html).to eq("<p>This is a <mark>highlight</mark>.</p>\n")
+    end
+
+    it "answers HTML table" do
+      html = renderer.call <<~CONTENT
+        | One| Two|
+        |---:|----|
+        |  1 |  2 |
+      CONTENT
+
+      expect(html).to eq(<<~PROOF)
+        <table><thead>
+        <tr>
+        <th style="text-align: right">One</th>
+        <th>Two</th>
+        </tr>
+        </thead><tbody>
+        <tr>
+        <td style="text-align: right">1</td>
+        <td>2</td>
+        </tr>
+        </tbody></table>
+      PROOF
+    end
   end
 end
