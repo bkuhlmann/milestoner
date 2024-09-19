2acbd4d46b41121f1c1c69f7787f377c46996f47
Brooke Kuhlmann <brooke@alchemists.io>
2024-09-19 14:03:05 -0600

    Added custom view scope builder
    Necessary because the default Hanami scope builder is completely broken because you can't use multiple scopes within the same partial which defaults the purpose and entire value of having custom scopes to begin with.
    
    This is also why this scope build is configured within the view to replace the default, broken behavior.
    
    Milestone: minor

--- lib/milestoner/views/milestones/show.rb
+++ lib/milestoner/views/milestones/show.rb
@@ -12,6 +12,7 @@ module Milestoner
         config.default_context = Context.new
         config.part_namespace = Parts
         config.paths = Container[:settings].build_template_paths
+        config.scope_builder = Milestoner::Views::ScopeBuilder
         config.template = "milestones/show"
 
         expose :tag
