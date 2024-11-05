800acffec297c435a24eef9a6e8a21cf856130e5
Brooke Kuhlmann <brooke@alchemists.io>
2024-11-05 16:34:33 -0700

    Added site build indexer
    This is extracted from the web builder in order to separate responsibilities and reduce complexity in the web builder by focusing on only building the index page. The web builder will soon be refactored to remove this duplication.
    
    Milestone: minor

---
+++ lib/milestoner/builders/site/indexer.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "dry/monads"
+
+module Milestoner
+  module Builders
+    module Site
+      # Builds web index.
+      class Indexer
+        include Dry::Monads[:result]
+        include Milestoner::Dependencies[:settings, :logger]
+
+        def initialize(path_resolver: PathResolver, view: Views::Milestones::Index.new, **)
+          super(**)
+          @path_resolver = path_resolver
+          @view = view
+        end
+
+        def call tags
+          return Success() unless settings.build_index
+
+          path_resolver.call settings.build_output.join("index.html"), logger: do |path|
+            path.write view.call(tags:, layout: settings.build_layout)
+          end
+        end
+
+        private
+
+        attr_reader :path_resolver, :view
+      end
+    end
+  end
+end
