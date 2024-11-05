8c13a43dc5bbd7ab409e85062bfd9a9e0ef6a7c5
Brooke Kuhlmann <brooke@alchemists.io>
2024-11-05 13:58:07 -0700

    Added index view
    Necessary to render the index view when building multiple milestones.
    
    Milestone: minor

---
+++ lib/milestoner/views/milestones/index.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require "hanami/view"
+
+module Milestoner
+  module Views
+    module Milestones
+      # The index view.
+      class Index < Hanami::View
+        include Dependencies[:settings]
+
+        config.default_context = Context.new
+        config.part_namespace = Parts
+        config.paths = Container[:settings].build_template_paths
+        config.scope_namespace = Scopes
+        config.scope_builder = Milestoner::Views::ScopeBuilder
+        config.template = "milestones/index"
+
+        expose :tags
+      end
+    end
+  end
+end
