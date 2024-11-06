f727f721ae2aa2130cbfb02784aa627d4aa390f0
Brooke Kuhlmann <brooke@alchemists.io>
2024-11-06 11:39:59 -0700

    Added HTML template past and future versions
    Necessary to enhance the viewing experience by providing a link back to the main versions index (root) as well as being able to navigate between previous and next versions. A popover was added to provide keyboard shortcut documentation/help.
    
    The `tag` remains exposed as `tag` instead of `present` in order to remain consistent with terminology used in the other build formats.
    
    Milestone: minor

--- spec/lib/milestoner/views/milestones/show_spec.rb
+++ spec/lib/milestoner/views/milestones/show_spec.rb
@@ -12,20 +12,24 @@ RSpec.describe Milestoner::Views::Milestones::Show do
   include_context "with enriched tag"
 
   describe "#call" do
+    let(:past) { tag.with version: "0.0.0" }
+    let(:present) { tag.with version: "0.1.0" }
+    let(:future) { tag.with version: "1.0.0" }
+
     it "includes label" do
-      expect(view.call(tag:).to_s).to include("Test")
+      expect(view.call(past:, tag: present, future:).to_s).to include("Test")
     end
 
     it "includes version" do
-      expect(view.call(tag:).to_s).to match(/\d+\.\d+\.\d+/)
+      expect(view.call(past:, tag: present, future:).to_s).to match(/\d+\.\d+\.\d+/)
     end
 
     it "includes URI" do
-      expect(view.call(tag:).to_s).to include("/projects/milestoner")
+      expect(view.call(past:, tag: present, future:).to_s).to include("/projects/milestoner")
     end
 
     it "includes commit" do
-      expect(view.call(tag:).to_s).to include("Added documentation")
+      expect(view.call(past:, tag: present, future:).to_s).to include("Added documentation")
     end
   end
 end
