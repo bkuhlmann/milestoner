76172fc6fec72fce1067212216fcb3339c982886
Brooke Kuhlmann <brooke@alchemists.io>
2024-11-13 16:39:04 -0700

    Added ASCII Doc pager
    This is extracted from the ASCII Doc builder in order to separate responsibilities and reduce complexity in the ASCII Doc builder by focusing on only building a single version. The ASCII Doc builder will soon be refactored to remove this duplication.
    
    Milestone: minor

---
+++ lib/milestoner/builders/adoc/pager.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Milestoner
+  module Builders
+    module ADoc
+      # Builds ASCII Doc version.
+      class Pager
+        include Milestoner::Dependencies[:settings, :logger]
+
+        def initialize(path_resolver: PathResolver, view: Views::Milestones::Show.new, **)
+          super(**)
+          @path_resolver = path_resolver
+          @view = view
+        end
+
+        def call past, tag, future
+          settings.project_version = tag.version
+          write past, tag, future
+        end
+
+        private
+
+        attr_reader :path_resolver, :view
+
+        def write past, tag, future
+          path = settings.build_output.join(tag.version, settings.build_basename).sub_ext ".adoc"
+
+          path_resolver.call path, logger: do
+            path.write view.call(past:, tag:, future:, layout: settings.build_layout, format: :adoc)
+          end
+        end
+      end
+    end
+  end
+end
